854c7c9f36eab866fdcf57cc42e52d8b3b9a8b3c
XmYx <miklos.mnagy@gmail.com>
2022-08-28 13:14:17 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
     'token': 'NTU3ODk3NjYzODM2NTg1OTg1.G0i08_.VbIwAMOJi2dR1YaCXwvDVTxmGb2xGJGdkZVrus',
-    'bot': 'StableBOT',
+    'bot': 'HerbalSpaceBot',
     'id': 557897663836585985,
     'prefix': '!'
 }
