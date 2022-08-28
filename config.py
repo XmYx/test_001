36a8cab4105f86473509f4d95fb5d410391f62bc
XmYx <miklos.mnagy@gmail.com>
2022-08-28 05:51:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'MTAxMDgxNDg3OTIwNjIzMjA5NQ.Gw3qI6.wX1BSwdB_MO2l5NrfYSb1X_5NT1wEPFsZpq_BA',
-    'bot': 'SD_test',
-    'id': 1010814879206232095,
+    'token': '79bbd5d22d3271bf9146d838e2e781cc3c05f6063fd8634ce01e7a7807184834',
+    'bot': 'StableBOT',
+    'id': 557897663836585985,
     'prefix': '!'
 }
